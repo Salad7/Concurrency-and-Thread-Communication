e8b19c715539920049f0a8ffdd220cdd41f9c705
salad7 <msalad@uncc.edu>
2016-12-22 14:10:10 -0500

    Implemented Runnable thread

---
+++ app/src/main/java/com/example/mohamed/threadconcur/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.mohamed.threadconcur;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Thread t = new Thread(new Worker());
+        t.run();
+
+    }
+}
---
+++ app/src/main/java/com/example/mohamed/threadconcur/Worker.java
@@ -0,0 +1,15 @@
+package com.example.mohamed.threadconcur;
+
+/**
+ * Created by mohamed on 12/22/16.
+ */
+
+public class Worker implements Runnable {
+
+    @Override
+    public void run() {
+        for(int i = 0; i < 100; i++){
+
+        }
+    }
+}
